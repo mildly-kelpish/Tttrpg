00cc7c7296a8af3d21aa1c40dfc5d40707732a2d
kelp <mildly_kelpish@proton.me>
2026-07-07 22:15:29 -0400

    Add files via upload

--- server.py
+++ server.py
@@ -1,9 +1,29 @@
-from starlette.applications import Starlette
-from starlette.responses import JSONResponse, PlainTextResponse
-from starlette.routing import Route
+import secrets
+import random
+from typing import Annotated
+from fastapi import FastAPI, Response, Cookie
+from pydantic import BaseModel
+from fastapi.responses import PlainTextResponse, JSONResponse
 from sqlmodel import Field, SQLModel, create_engine, Session, select
-import tomllib # apparently, toml is included in python now
-import json
+import tomllib  # apparently, toml is included in python now
+
+
+app = FastAPI()
+dmkey = secrets.token_urlsafe(12)
+playerkeys = [
+    "UNSAFEKEY",
+    secrets.token_urlsafe(12),
+    secrets.token_urlsafe(12),
+    secrets.token_urlsafe(12),
+    secrets.token_urlsafe(12),
+    secrets.token_urlsafe(12),
+    secrets.token_urlsafe(12),
+    secrets.token_urlsafe(12),
+    secrets.token_urlsafe(12),
+    secrets.token_urlsafe(12),
+]
+# THERE HAS GOT TO BE A BETTER WAY TO DO THIS !!
+
 
 class idlookup(SQLModel, table=True):
     # standard item id types
@@ -18,63 +38,132 @@ class idlookup(SQLModel, table=True):
     lookupid: int
     name: str
     stats: str
-    authket: str | None = None
+
 
 engine = create_engine("sqlite:///world.db", echo=True)
 SQLModel.metadata.create_all(engine)
 
-with open('config.toml', 'rb') as f:
+with open("config.toml", "rb") as f:
     config = tomllib.load(f)
 
-with open('config.json', 'r') as file:
-    config = json.load(file)
 
-mapping = [[0,0,0,0,0],
-           [0,0,0,0,0],
-           [0,0,0,0,0],
-           [0,0,0,0,0],
-           [0,0,0,0,0]]
+class authenticateconf(BaseModel):
+    key: str  # the key(s) the client(s) will send
+    typ: int  # one of   1: DM_KEY   2: [playerkeys]
+
+
+class sendmapping(BaseModel):
+    mappng: list
+
+
+class sendobjdef(BaseModel):
+    names: str
+    status: str
+
+
+mapping = [
+    [0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0],
+]
+
 
 async def objlookupfunc(lookup: int):
     with Session(engine) as session:
-        statement = select(idlookup).where(idlookup.lookupid == lookup )
+        statement = select(idlookup).where(idlookup.lookupid == lookup)
         results = session.exec(statement)
         return results.first().model_dump()
-async def connect(request): 
-    return JSONResponse({'hello' : 'world'})
 
-async def objlookup(request):
-    objid = request.path_params['objid']
+
+async def objcreationfunc(
+    making: int,
+    nam: str,
+    state: str,
+):
+    with Session(engine) as session:
+        session.add(idlookup(lookupid=making, name=nam, stats=state))
+        session.commit()
+
+
+@app.get("/")
+async def connect():
+    return JSONResponse({"hello": "world"})
+
+
+@app.get("/info/{objid}")
+async def objlookup(objid):
     objectfinal = await objlookupfunc(objid)
-    return PlainTextResponse(f"name=\"{objectfinal["name"]}\"\n{objectfinal["stats"].replace('\\n', '\n')}")
-async def teapot(request):
-    return PlainTextResponse(status_code=418)    
-
-async def players(request):
-    print("one")
-
-async def dm(request):
-    print("two")        
-
-async def mape(request):
-    typ = request.method
-    if typ == "GET":
-        response=str(mapping)
-    if typ == "POST":
-        part1 = await request.json()
-        
+    return PlainTextResponse(
+        f'name="{objectfinal["name"]}"\n{objectfinal["stats"].replace("\\n", "\n")}'
+    )
 
 
-    return PlainTextResponse(response)
+@app.post("/make/{objid}")
+async def objmake(
+    objid, objeclass: sendobjdef, AUTH: Annotated[str | None, Cookie()] = None
+):
+    """create an object, simple enough"""
+    if AUTH == dmkey:
+        await objcreationfunc(objid, objeclass.names, objeclass.status)
+        return PlainTextResponse("succesfully created!")
 
-async def authenticatething(request):
-    
+
+@app.get("/coffee")
+async def teapot():
+    """this is in entirely as a joke, clients can do whatever with it"""
+    return PlainTextResponse(status_code=418)
 
 
-app = Starlette(debug=True, routes=[
-    Route('/', connect),
-    Route('/info/{objid:int}', objlookup),
-    Route('/coffee', teapot),
-    Route('/map', mape, methods=["GET", "POST"]),
-    Route('/authenticate', authenticatething)
-])
+@app.get("/map")
+async def mapget():
+    return PlainTextResponse(str(mapping))
+
+
+@app.post("/map")
+async def mapset(mappings: sendmapping, AUTH: Annotated[str | None, Cookie()] = None):
+    """POST an array to this to set the map that will be sent by GET requests to /map!"""
+    if AUTH == dmkey:
+        global mapping
+        mapping = mappings.mappng
+        print(mapping)
+        return PlainTextResponse(str(mapping))
+
+
+@app.post("/authenticate")
+async def authenticatething(authenticator: authenticateconf):
+    """client will send a POST request containing what type of user it wants to authenticate as AS WELL as the (configured on server) key for said user\n
+    why not just use Oauth?  cause i dont understand how to and im kindof scared of it"""
+    if authenticator.typ == 1:
+        if authenticator.key == config["DM_KEY"]:
+            content = {"log": "Succesfully authenticated as DM"}
+            response = JSONResponse(content=content)
+            response.set_cookie(
+                key="AUTH", value=dmkey, max_age=14400
+            )  # authentication stuff lasts for exactly 4 hours before you have to set a new one
+            return response
+        else:
+            return PlainTextResponse("FAILED AUTHENTICATION", status_code=401)
+    if authenticator.typ == 2:
+        try:
+            if config["playerkeys"][authenticator.key] in range(10):
+                content = {
+                    "log": f"Succesfully authenticated as player {config['playerkeys'][authenticator.key]}"
+                }
+                response = JSONResponse(content=content)
+                response.set_cookie(
+                    key="PAUTH",
+                    value=playerkeys[config["playerkeys"][authenticator.key]],
+                    max_age=14400,
+                )
+                return response
+            else:
+                return PlainTextResponse(
+                    "FAILED AUTHENTICATION", status_code=401
+                )  # how are you going to even reach this??
+        except:
+            return PlainTextResponse(
+                "internal server error, its likely you used the wrong key!",
+                status_code=401,
+            )
--- testclient.py
+++ testclient.py
@@ -1,5 +1,6 @@
 # THIS IS AN EXAMPLE CLIENT
-# you can probably use it, but its more of here to be an example and for me to make sure my code actually works 
+# you can probably use it, but its more of here to be an example and for me to make sure my code actually works
 
 import requests
-r = requests.post('http://127.0.0.1:8000/map', data="test")
+
+r = requests.post("http://127.0.0.1:8000/map", data="test")
